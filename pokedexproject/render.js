b250b8d05c9090ec5c74c38d7b61c1ad7cce4a73
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-06-03 16:32:56 +0200

    refactoring

--- pokedexproject/render.js
+++ pokedexproject/render.js
@@ -1,53 +1,62 @@
-function init() {
-    renderPokemon();
+async function init() {
+    await getUrlForPokemon();
+
+};
+
+
+let detailsAsPokemon = [];
+let allPokemon = [];
+let resultAsJSON = [];
+let UrlPackages = [];
+
+let firstUrlset = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
+
+
+
+async function getUrlForPokemon() {
+    if(firstUrlset){
+    let test = await fetch(firstUrlset);
+    let testresult = await test.json();
+    getAllPokemon(testresult);
+    } else{
+        let test2 = await fetch(nextUrlset);
+        let testAsJson = await test2.json();
+        getAllPokemon(testAsJson);
+    }
+
+    console.log(testresult);
+
 };
 
 
 
+async function getAllPokemon(testresult, testAsJson) {
 
+    for (let getPokemonIndex = 0; getPokemonIndex < testresult.results.length; getPokemonIndex++) {
 
-let basicUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20';
+        let singelUrl = await fetch(testresult.results[getPokemonIndex].url);
+        let singlePokemonInJson = await singelUrl.json();
+        allPokemon.push(singlePokemonInJson);
 
+    }
+    console.log(allPokemon);
+    renderFirstPokemon(allPokemon);
+    await getNextPokemonpackage(testresult);
 
+};
 
+async function getNextPokemonpackage(testresult) {
+    let nextUrl = testresult.next;
 
-async function renderPokemon() {
+    let nextPackage = await fetch(nextUrl);
+    let resultAsJson = await nextPackage.json();
+    console.log(resultAsJson);
+    let nextUrlset = resultAsJson.next;
+    console.log(nextUrlset);
 
-    let result = await fetch(basicUrl); //let result
-
-    let resultAsJSON = await result.json();       //
-    let pokemon = resultAsJSON.results;      //let pokemon = resultAsJson.results
-    console.log(pokemon);
-
-
-    let mainsectioncontent = document.getElementById('main-section-content');  //let mainsectioncontent nennen
-    // auch value nennen / oder durch einen neuen Loop Zahlen einfügen
-
-
-
-    for (let imageIndex = 1; imageIndex <= 20; imageIndex++) {
-        let detailsUrl = `https://pokeapi.co/api/v2/pokemon/${imageIndex}/`;
-        let details = await fetch(detailsUrl);
-        let detailsAsJSON = await details.json();
-
-        console.log(detailsAsJSON);
-        mainsectioncontent.innerHTML += `
-        <div class="pokemoncontainer">
-        <div class="pokemonName">
-        <h4>#${imageIndex}</h4> <h3>${pokemon[imageIndex - 1].name}</h3>
-        </div>
-        <div class="ImageContainer ${detailsAsJSON.types[0].type.name}-Img">
-        <img src="${detailsAsJSON.sprites.other.dream_world['front_default']}">
-        </div>
-        <div class="type-button-area">
-        <img class="${detailsAsJSON.types[0].type.name}"src="./images/icons/${detailsAsJSON.types[0].type.name}.svg">
-        ${detailsAsJSON.types[1] ? `<img class="${detailsAsJSON.types[1].type.name}" src="./images/icons/${detailsAsJSON.types[1].type.name}.svg">` : ''}   
-        </div>
-        </div>`
-
-    };
 
 }
+
 //here we see a short if-statement within a literals start with an ${} and put a literals again in the if statement within the literals
 //so it's possible to put a literals within a literals
 
@@ -59,29 +68,57 @@ async function renderPokemon() {
 
 //VM im Debugger steht für Virtual Maschine
 
-async function renderNext() {
-    
+
+function renderFirstPokemon(allPokemon) {
+
     let mainsectioncontent = document.getElementById('main-section-content');
+    for (let firstPokemonIndex = 0; firstPokemonIndex < allPokemon.length; firstPokemonIndex++) {
 
-    for (let nextPokemonIndex = 21; nextPokemonIndex <= 40; nextPokemonIndex++) {
-
-        let loadNextUrl = `https://pokeapi.co/api/v2/pokemon/${nextPokemonIndex}/`;
-        let loadnext = await fetch(loadNextUrl);
-        let detailsAsPokemon = await loadnext.json();
-
-        console.log(detailsAsPokemon);
-
-        mainsectioncontent.innerHTML += `<div class="pokemoncontainer">
+        mainsectioncontent.innerHTML += `
+        <div class="pokemoncontainer">
         <div class="pokemonName">
-        <h4>#${nextPokemonIndex}</h4> <h3>${detailsAsPokemon.name}</h3>
+        <h4>#${firstPokemonIndex + 1}</h4> <h3>${allPokemon[firstPokemonIndex].name}</h3>
         </div>
-        <div class="ImageContainer ${detailsAsPokemon.types[0].type.name}-Img">
-        <img src="${detailsAsPokemon.sprites.other.dream_world['front_default']}">
+        <div class="ImageContainer ${allPokemon[firstPokemonIndex]}-Img">
+        <img src="${allPokemon[firstPokemonIndex].sprites.other.dream_world['front_default']}">
         </div>
         <div class="type-button-area">
-        <img class="${detailsAsPokemon.types[0].type.name}"src="./images/icons/${detailsAsPokemon.types[0].type.name}.svg">
-        ${detailsAsPokemon.types[1] ? `<img class="${detailsAsPokemon.types[1].type.name}" src="./images/icons/${detailsAsPokemon.types[1].type.name}.svg">` : ''}   
+        <img class="${allPokemon[firstPokemonIndex].types[0].type.name}"src="./images/icons/${allPokemon[firstPokemonIndex].types[0].type.name}.svg">
+        ${allPokemon[firstPokemonIndex].types[1] ? `<img class="${allPokemon[firstPokemonIndex].types[1].type.name}" src="./images/icons/${allPokemon[firstPokemonIndex].types[1].type.name}.svg">` : ''}   
         </div>
         </div>`
     }
+}
+
+
+
+function renderNext(detailsAsPokemon, nextPokemonIndex) {
+
+    let mainsectioncontent = document.getElementById('main-section-content');
+
+    for (let nextPokemonIndex2 = 0; nextPokemonIndex2 <= 20; nextPokemonIndex2++) {
+
+        mainsectioncontent.innerHTML += `<div class="pokemoncontainer">
+        <div class="pokemonName">
+        <h4>#${nextPokemonIndex2 + 21}</h4> <h3>${detailsAsPokemon[nextPokemonIndex2].name}</h3>
+        </div>
+        <div class="ImageContainer ${detailsAsPokemon[nextPokemonIndex2].name}-Img">
+        <img src="${detailsAsPokemon[nextPokemonIndex2].sprites.other.dream_world['front_default']}">
+        </div>
+        <div class="type-button-area">
+        <img class="${detailsAsPokemon[nextPokemonIndex2].types[0].type.name}"src="./images/icons/${detailsAsPokemon[nextPokemonIndex2].types[0].type.name}.svg">
+        ${detailsAsPokemon[nextPokemonIndex2].types[1] ? `<img class="${detailsAsPokemon[nextPokemonIndex2].types[1].type.name}" src="./images/icons/${detailsAsPokemon[nextPokemonIndex2].types[1].type.name}.svg">` : ''}   
+        </div>
+        </div>`
+    } console.log(detailsAsPokemon);
 };
+
+
+
+
+
+
+
+
+//side note: eine Domain ist nur eine andere schreibweise einer IP-Adresse
+//DNS = Domain Name System
