82a5f0b6958728f8d8830f51eb21a3d96a173921
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-06-14 00:03:17 +0200

    set up basics for Overlay

--- pokedexproject/pokemonOverlayWindow.js
+++ pokedexproject/pokemonOverlayWindow.js
@@ -1,13 +1,12 @@
 
-function toggleOverlayWindow(firstPokemonIndex) {
+function toggleOverlayWindow() {
     let Overlay = document.getElementById('OverlayWindow')
     Overlay.classList.toggle('dont_show')
-    overlayWindowdiv(firstPokemonIndex);
 };
 
 function overlayWindowdiv(firstPokemonIndex) {
     let conectionToOverlaydiv = document.getElementById('OverlayWindow')
-    conectionToOverlaydiv.innerHTML = `<div class="pokemonNameOverlay">
+    conectionToOverlaydiv.innerHTML = `<div  class="pokemonNameOverlay">
     <h4>#${firstPokemonIndex + 1}</h4> <h3>${allPokemon[firstPokemonIndex].name}</h3>
     </div>
     <div class="ImageContainerOverlay ${allPokemon[firstPokemonIndex].types[0].type.name}-Img">
@@ -15,6 +14,26 @@ function overlayWindowdiv(firstPokemonIndex) {
     </div>
     <div class="mainInfoCategoriesOverlay">
     <h4>main</h4><hr><h4>stats</h4><hr><h4>evo</h4>
+    </div>
+    <div class="pokemondetails">
+    <table>
+    <tr>
+    <td><h3>Height</h3></td>
+    <td><h3>:1,34m</h3></td>
+    </tr>
+    <tr>
+    <td><h3>weight</h3></td>
+    <td><h3>:8,5kg</h3></td>
+    </tr>
+    <tr>
+    <td><h3>Base esperience</h3></td>
+    <td><h3>:abc</h3></td>
+    </tr>
+    <tr>
+    <td><h3>abilitys</h3></td>
+    <td><h3>:abc</h3></td>
+    </tr>
+    </table>
     </div>
    `
     console.log('funktion arbeitet');
