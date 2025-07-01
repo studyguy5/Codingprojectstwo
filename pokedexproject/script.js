c227795d872ecb44a909030e7c969174f20a0869
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-07-01 15:17:27 +0200

    keep up to date

--- pokedexproject/script.js
+++ pokedexproject/script.js
@@ -2,18 +2,21 @@ let allPokemon = [];
 let firstload = true;
 let firstLoad = true;
 let packageresultAsJson = [];
-let currentStartNumber = 20;
+let currentStartNumber = 19;
 let offset = 20;
 let limit = 20;
+let evoOffset = 10;
+let evoLimit = 10;
+let allEvoChains = [];
 
 async function init() {
     await getUrlForPokemon();
-    
+
 };
 
 
 let firstUrlset = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
-let secondUrlset = 'https://pokeapi.co/api/v2/pokemon?limit=40&offset=20';
+let evolutionchainUrlset = 'https://pokeapi.co/api/v2/evolution-chain?limit=10&offset=0';
 
 
 async function getUrlForPokemon() {
@@ -27,15 +30,18 @@ async function getUrlForPokemon() {
 };
 
 
+
+
 async function getNextUrlSet() {
-    if(offset > 1299){
-        limit =2 
-    }else{
+    if (offset > 1299) {
+        limit = 2
+    } else {
         let fetchNextPackage = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);            //fetch the Url Package ich muss das hier dynamisch machen
         let nextUrlAsJson = await fetchNextPackage.json();              //transform into json
         //let nextUrlContainer = UrlAsJson.next;                  //next Url here offset 20 limit 40
         getAllPokemon(nextUrlAsJson);
-        offset += 20;}     
+        offset += 20;
+    }
 }
 
 async function getAllPokemon(packageresultAsJson) {     //here we fetch each single Url of the package
@@ -49,7 +55,7 @@ async function getAllPokemon(packageresultAsJson) {     //here we fetch each sin
         renderFirstPokemon();
         firstload = false;
     } else {
-        renderNextPokemon(allPokemon);   
+        renderNextPokemon(allPokemon);
     }
 }
 
@@ -63,10 +69,9 @@ async function getAllPokemon(packageresultAsJson) {     //here we fetch each sin
 
 function renderFirstPokemon() {
     let mainsectioncontent = document.getElementById('main-section-content');
-    for (let firstPokemonIndex = 0; firstPokemonIndex < 20; firstPokemonIndex++) {
-
+    for (let firstPokemonIndex = 0; firstPokemonIndex < 19; firstPokemonIndex++) {
         mainsectioncontent.innerHTML += `
-        <div onclick="overlayWindowdiv(${firstPokemonIndex}); toggleOverlayWindow()" class="pokemoncontainer">
+        <div onclick="overlayWindowdiv(${firstPokemonIndex}); toggleOverlayWindow(${firstPokemonIndex})" class="pokemoncontainer">
         <div class="pokemonName">
         <h4>#${firstPokemonIndex + 1}</h4> <h3>${allPokemon[firstPokemonIndex].name}</h3>
         </div>
@@ -78,33 +83,62 @@ function renderFirstPokemon() {
         ${allPokemon[firstPokemonIndex].types[1] ? `<img class="${allPokemon[firstPokemonIndex].types[1].type.name}" src="./images/icons/${allPokemon[firstPokemonIndex].types[1].type.name}.svg">` : ''}   
         </div>
         </div>`
-    }//here we see a short if-statement within a literals start with an ${} and put a literals again in the if statement within the literals
+
+    } getEvolutionChainFromPokemon();
+
+    //here we see a short if-statement within a literals start with an ${} and put a literals again in the if statement within the literals
 };//so it's possible to put a literals within a literals
 
 
 
 function renderNextPokemon(allPokemon) {
     let mainsectioncontent = document.getElementById('main-section-content');
-    //for (let index = 20; index < resultAsJson.length; index+=20) {
-
-    for (let i = currentStartNumber; i < currentStartNumber + 20; i++) {        //variable- currentStartNumbe is seted in head area, so instead of a number, there is placed a variable in the loop as start
-                                                                                //also +1 added to the index, because it starts at 0
-        mainsectioncontent.innerHTML += `<div class="pokemoncontainer">
+    for (let cs = currentStartNumber; cs < currentStartNumber + 20; cs++) {        //variable- currentStartNumbe is seted in head area, so instead of a number, there is placed a variable in the loop as start
+        //also +1 added to the index, because it starts at 0
+        mainsectioncontent.innerHTML += `
+        <div onclick="NextoverlayWindowdiv(${cs}); toggleOverlayWindow(${cs})" class="pokemoncontainer">
         <div class="pokemonName">
-        <h4>#${i+1}</h4> <h3>${allPokemon[i].name}</h3>                         
+        <h4>#${cs + 1}</h4> <h3>${allPokemon[cs].name}</h3>                         
         </div>
-        <div class="ImageContainer ${allPokemon[i].types[0].type.name}-Img">
-        <img src="${allPokemon[i].sprites.other.dream_world['front_default']}">
+        <div class="ImageContainer ${allPokemon[cs].types[0].type.name}-Img">
+        <img src="${allPokemon[cs].sprites.other.dream_world['front_default']}">
         </div>
         <div class="type-button-area">
-        <img class="${allPokemon[i].types[0].type.name}"src="./images/icons/${allPokemon[i].types[0].type.name}.svg">
-        ${allPokemon[i].types[1] ? `<img class="${allPokemon[i].types[1].type.name}" src="./images/icons/${allPokemon[i].types[1].type.name}.svg">` : ''}   
+        <img class="${allPokemon[cs].types[0].type.name}"src="./images/icons/${allPokemon[cs].types[0].type.name}.svg">
+        ${allPokemon[cs].types[1] ? `<img class="${allPokemon[cs].types[1].type.name}" src="./images/icons/${allPokemon[cs].types[1].type.name}.svg">` : ''}   
         </div>
         </div>`
     }
     currentStartNumber += 20;   // at the end of 20 rounds to the currentStartNumber is added 20, which is the next start
+    // getNextEvolutionChainFromPokemon();
     console.log(allPokemon);
 };
+
+async function getEvolutionChainFromPokemon() {
+    let evoUrl = await fetch(evolutionchainUrlset);
+    let evoUrlresponse = await evoUrl.json();
+    console.log(evoUrlresponse);
+    getAllEvoChains(evoUrlresponse);
+};
+
+// async function getNextEvolutionChainFromPokemon() {     //first fetchstep for names
+//     let evoUrl = await fetch(`https://pokeapi.co/api/v2/evolution-chain?offset=${evoOffset}&limit=${evoLimit}`);
+//     let evoUrlresponse = await evoUrl.json();
+//     console.log(evoUrlresponse);
+//     getAllEvoChains(evoUrlresponse);
+//     offset += 10;
+// };
+
+async function getAllEvoChains(evoUrlresponse) {                         //second fetchstep for names
+    for (let index = 0; index < evoUrlresponse.results.length; index++) {
+        let nextfetch = await fetch(evoUrlresponse.results[index].url);
+        let singleUrlAsJson = await nextfetch.json();
+        console.log(singleUrlAsJson);
+        allEvoChains.push(singleUrlAsJson);
+
+    }
+}
+
 
 
 //side note: eine Domain ist nur eine andere schreibweise einer IP-Adresse
