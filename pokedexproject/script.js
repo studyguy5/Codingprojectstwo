c9a51e7455de00e9bb8deadab41c4e45fa8c49a5
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-07-16 01:12:45 +0200

    add basic stopPropagation

--- pokedexproject/script.js
+++ pokedexproject/script.js
@@ -11,30 +11,47 @@ let allEvoChains = [];
 
 async function init() {
     await getUrlForPokemon();
-
 };
 
+async function filterArray(){
+    let connection = document.getElementById('filterinput').value.toLowerCase();
+    if(connection.length > 2){
+    let filtertest = allPokemon.filter(allPokemon =>allPokemon.name.toLowerCase().includes(connection))
+    console.log(filtertest);}
+}
+
+function toggleOverlayWrapper() {
+    let Overlay = document.getElementById('Overlaywrapper')
+    Overlay.classList.toggle('dont_show')
+    
+};
+
+function stopProp(event){
+    event.stopPropagation();
+}
 
 let firstUrlset = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
 let evolutionchainUrlset = 'https://pokeapi.co/api/v2/evolution-chain?limit=10&offset=0';
 
 
 async function getUrlForPokemon() {
+    showLoadingAnimation();
     let fetchFirstPackage = await fetch(firstUrlset);            //fetch the Url Package
     let packageresultAsJson = await fetchFirstPackage.json();       //transform into json
     await getAllPokemon(packageresultAsJson);              // testresult and testAsJson migrate into getAllPokemon, we need only one paramter in getAllPokemon
-    console.log(packageresultAsJson);
-
-
-    console.log(allPokemon);
 };
 
 
-
+// document.getElementById('buttonId').disabled = false;
 
 async function getNextUrlSet() {
+    document.getElementById('buttonId').disabled = true;
+    showLoadingAnimation();
     if (offset > 1299) {
-        limit = 2
+        let fetchNextPackage = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=2`);            //fetch the Url Package ich muss das hier dynamisch machen
+        let nextUrlAsJson = await fetchNextPackage.json();              //transform into json
+        //let nextUrlContainer = UrlAsJson.next;                  //next Url here offset 20 limit 40
+        getAllPokemon(nextUrlAsJson);
     } else {
         let fetchNextPackage = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);            //fetch the Url Package ich muss das hier dynamisch machen
         let nextUrlAsJson = await fetchNextPackage.json();              //transform into json
@@ -46,16 +63,19 @@ async function getNextUrlSet() {
 
 async function getAllPokemon(packageresultAsJson) {     //here we fetch each single Url of the package
     for (let getPokemonIndex = 0; getPokemonIndex < packageresultAsJson.results.length; getPokemonIndex++) {
-
         let singelUrl = await fetch(packageresultAsJson.results[getPokemonIndex].url);
         let singlePokemonInJson = await singelUrl.json();
         allPokemon.push(singlePokemonInJson);
     };
     if (firstload) {
+        hidetoggleAnimation();
         renderFirstPokemon();
+        document.getElementById('buttonId').disabled = false;
         firstload = false;
     } else {
+        hidetoggleAnimation();
         renderNextPokemon(allPokemon);
+        document.getElementById('buttonId').disabled = false;
     }
 }
 
@@ -71,7 +91,7 @@ function renderFirstPokemon() {
     let mainsectioncontent = document.getElementById('main-section-content');
     for (let firstPokemonIndex = 0; firstPokemonIndex < 19; firstPokemonIndex++) {
         mainsectioncontent.innerHTML += `
-        <div onclick="overlayWindowdiv(${firstPokemonIndex}); toggleOverlayWindow(${firstPokemonIndex})" class="pokemoncontainer">
+        <div onclick="overlayWindowdiv(${firstPokemonIndex}); toggleOverlayWrapper(${firstPokemonIndex})" class="pokemoncontainer">
         <div class="pokemonName">
         <h4>#${firstPokemonIndex + 1}</h4> <h3>${allPokemon[firstPokemonIndex].name}</h3>
         </div>
@@ -96,7 +116,7 @@ function renderNextPokemon(allPokemon) {
     for (let cs = currentStartNumber; cs < currentStartNumber + 20; cs++) {        //variable- currentStartNumbe is seted in head area, so instead of a number, there is placed a variable in the loop as start
         //also +1 added to the index, because it starts at 0
         mainsectioncontent.innerHTML += `
-        <div onclick="NextoverlayWindowdiv(${cs}); toggleOverlayWindow(${cs})" class="pokemoncontainer">
+        <div onclick="NextoverlayWindowdiv(${cs}); toggleOverlayWrapper(${cs})" class="pokemoncontainer">
         <div class="pokemonName">
         <h4>#${cs + 1}</h4> <h3>${allPokemon[cs].name}</h3>                         
         </div>
@@ -143,3 +163,24 @@ async function getAllEvoChains(evoUrlresponse) {                         //secon
 
 //side note: eine Domain ist nur eine andere schreibweise einer IP-Adresse
 //DNS = Domain Name System
+
+function showLoadingAnimation(){
+    let loadingAnimation = document.getElementById('loadingSpinner')
+    loadingAnimation.classList.remove('dont-Show')
+};
+
+function hidetoggleAnimation(){
+    let toggleCont = document.getElementById('loadingSpinner')
+    toggleCont.classList.add('dont-Show')
+}
+
+
+
+
+
+// const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+// const result = pokemonList.filter((pokemon) => pokemon.name.includes("abc"));
+
+// console.log(result);
+// // Expected output: Array ["exuberant", "destruction", "present"]
