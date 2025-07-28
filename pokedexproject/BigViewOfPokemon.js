85fad6662ae4fcc1a6c386c00432e9a18974c76d
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-07-28 15:52:39 +0200

    deleted 'nexttwenty....js' and use BigViewPokemon.js for all Pokemon insteat

---
+++ pokedexproject/BigViewOfPokemon.js
@@ -0,0 +1,87 @@
+let firstfetchArray = [];
+let secondfetchArray = [];
+let mainDataArrayWord = ["height", "weight", "base_experience", "abilities", "abilities"];
+let stats = ['0', '1', '2', '4', '5']//try to put the stats into an array
+let ImageResultArray = [];
+
+function overlayWindowdiv(firstPokemonIndex) {
+    let conectionToOverlaydiv = document.getElementById('OverlayWindow')
+    conectionToOverlaydiv.innerHTML = renderOverlayDiv(firstPokemonIndex);
+    showMain(firstPokemonIndex, mainDataArrayWord);
+};
+
+
+function skipToNext(currentIndex) {
+    currentIndex++
+    overlayWindowdiv(currentIndex);
+}
+
+function skipToPrevious(currentIndex) {
+    currentIndex--
+    overlayWindowdiv(currentIndex);
+}
+
+
+function showMain(currentIndex, mainDataArrayWord) {
+    let maincontainer = document.getElementById('pokemondetails')
+    for (let mainDataIndex = 0; mainDataIndex < mainDataArrayWord.length; mainDataIndex++) {
+        let value3 = allPokemon[currentIndex][mainDataArrayWord[3]][0].ability.name;
+        let value4 = allPokemon[currentIndex][mainDataArrayWord[4]][1] ? ', ' + allPokemon[currentIndex][mainDataArrayWord[4]][1].ability.name : "";
+        maincontainer.innerHTML += renderMainData(mainDataIndex, currentIndex, mainDataArrayWord, value3, value4)
+    }
+    console.log('Funktion: showmain')
+};
+
+function showstats(currentIndex) {
+    let textX = (10 + 150) / 2;
+    let textY = (25 + 30) / 2;
+    let statscontainer = document.getElementById('pokemondetails')
+    statscontainer.innerHTML = "";
+    for (let statsIndex = 0; statsIndex < stats.length; statsIndex++) {
+        statscontainer.innerHTML += renderStats(currentIndex, statsIndex, textX, textY)
+    }
+};
+
+async function showEvochain(currentIndex) {
+    ImageResultArray = [];
+    firstfetchArray = [];
+    secondfetchArray = [];
+    let evoChainAsJson = await getEvoChain(currentIndex);
+    let nameArray = [
+        evoChainAsJson.chain.species.name,
+        evoChainAsJson.chain.evolves_to[0].species.name,
+        evoChainAsJson.chain.evolves_to[0].evolves_to[0] ? evoChainAsJson.chain.evolves_to[0].evolves_to[0].species.name : null
+    ]
+    let realName = nameArray.filter(name => name !== null);
+    await fetchImageOfEvo(realName);
+    await showEvoImage(evoChainAsJson, ImageResultArray);
+};
+
+async function getEvoChain(currentIndex) {
+    let evoUrl = `https://pokeapi.co/api/v2/pokemon-species/${currentIndex + 1}/`
+    let evoFetch = await fetch(evoUrl);
+    let resultAsJson = await evoFetch.json();
+    let secondFetch = await fetch(resultAsJson.evolution_chain.url);
+    let evoChainAsJson = await secondFetch.json();  //here we have all names of the chain but not the pictures
+    return evoChainAsJson;
+}
+
+async function fetchImageOfEvo(realName) {
+    
+    for (let ImageIndex = 0; ImageIndex < realName.length; ImageIndex++) {
+        let pokemonImageUrl = `https://pokeapi.co/api/v2/pokemon/${realName[ImageIndex]}/`;
+        let fetchEvoImage = await fetch(pokemonImageUrl);
+        let ImageResultAsJson = await fetchEvoImage.json();
+        ImageResultArray.push(ImageResultAsJson);
+    }
+}
+
+async function showEvoImage(evoChainAsJson, ImageResultArray) {
+    let evocontainer = document.getElementById('pokemondetails')
+    evocontainer.innerHTML = renderEvoImage(evoChainAsJson, ImageResultArray)
+};
+
+// window.toggleOverlayWindow = toggleOverlayWindow;  von rechts nach links soll die funktion toggleOverlayWindow mit dem Window tag global machen
+
+
+
